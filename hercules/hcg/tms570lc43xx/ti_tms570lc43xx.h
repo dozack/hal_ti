4e001489d91e1e0ae8b4186d8bdb53ac7f2305e7
Zoltan Dolensky <z.dolensky@gmail.com>
2023-12-24 17:23:03 +0100

    add halcogen files for tms570lc43x

--- hercules/hcg/tms570lc43xx/ti_tms570lc43xx.h
+++ hercules/hcg/tms570lc43xx/ti_tms570lc43xx.h
@@ -1,4 +1,47 @@
 #ifndef TI_TMS570LC43XX_HAL
 #define TI_TMS570LC43XX_HAL
 
+#include "include/sys_common.h"
+
+#include "include/HL_reg_adc.h"
+#include "include/HL_reg_can.h"
+#include "include/HL_reg_crc.h"
+#include "include/HL_reg_dcc.h"
+#include "include/HL_reg_dma.h"
+#include "include/HL_reg_ecap.h"
+#include "include/HL_reg_efc.h"
+#include "include/HL_reg_eqep.h"
+#include "include/HL_reg_esm.h"
+#include "include/HL_reg_etpwm.h"
+#include "include/HL_reg_flash.h"
+#include "include/HL_reg_gio.h"
+#include "include/HL_reg_het.h"
+#include "include/HL_reg_htu.h"
+#include "include/HL_reg_i2c.h"
+#include "include/HL_reg_lin.h"
+#include "include/HL_reg_mibspi.h"
+#include "include/HL_reg_pbist.h"
+#include "include/HL_reg_pcr.h"
+#include "include/HL_reg_pinmux.h"
+#include "include/HL_reg_pmm.h"
+#include "include/HL_reg_pom.h"
+#include "include/HL_reg_rti.h"
+#include "include/HL_reg_sci.h"
+#include "include/HL_reg_spi.h"
+#include "include/HL_reg_stc.h"
+#include "include/HL_reg_system.h"
+#include "include/HL_reg_vim.h"
+
+#include "include/HL_errata.h"
+#include "include/HL_errata_SSWF021_45_defs.h"
+#include "include/HL_errata_SSWF021_45.h"
+
+#include "include/sys_core.h"
+#include "include/sys_dma.h"
+#include "include/sys_pcr.h"
+#include "include/sys_pmu.h"
+#include "include/sys_selftest.h"
+#include "include/sys_vim.h"
+#include "include/system.h"
+
 #endif
